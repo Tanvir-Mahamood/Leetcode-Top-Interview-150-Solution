9cfee82c568d4b28147de255bf39846309ead617
Tanvir Mahamood <deltatanvir2002@gmail.com>
2025-12-02 20:37:55 +0600

    Time: 0 ms (100.00%) | Memory: 9 MB (11.64%) - LeetSync

--- 290-word-pattern/word-pattern.cpp
+++ 290-word-pattern/word-pattern.cpp
@@ -5,31 +5,29 @@ public:
         string word;
         while(iss>>word)
             words.push_back(word);
-        
     }
 
     bool wordPattern(string pattern, string s) {
-        map<char, string> mp;
+        vector<string> mp(26, "");
         map<string, int> cnt;
-        vector<string> words;
 
+        vector<string> words;
         stringToWords(s, words);
 
         if(pattern.size() != words.size()) return false;
         
-        int i;
-        char ch;
+        int i, ch;
         string word;
         for(i=0; i<pattern.size(); i++) {
-            ch = pattern[i];
+            ch = pattern[i] - 'a';
             word = words[i];
 
-            if(!mp.count(ch) && !cnt[word]) {
+            if(mp[ch] == "" && !cnt[word]) {
                 mp[ch] = word;
                 cnt[word]++;
             }
-            else if(!mp.count(ch)) return false;
-            else if(mp.count(ch) && mp[ch] != word) return false;
+            else if(mp[ch] == "") return false;
+            else if(mp[ch] != "" && mp[ch] != word) return false;
         }
         return true;
     }
