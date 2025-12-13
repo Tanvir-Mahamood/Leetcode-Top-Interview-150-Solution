cf24674e297f95ae193a98ac79c4e16993f8b4e0
Tanvir Mahamood <deltatanvir2002@gmail.com>
2025-12-13 21:12:49 +0600

    Time: 0 ms (100.00%) | Memory: 9.5 MB (60.52%) - LeetSync

--- 289-game-of-life/game-of-life.cpp
+++ 289-game-of-life/game-of-life.cpp
@@ -1,57 +1,42 @@
 class Solution {
 public:
-    bool isValid(int i, int j, int m, int n) {
-        if(i<0 || i >= m) return false;
-        if(j<0 || j >= n) return false;
-        return true;
+    int m, n;
+
+    bool isValid(int i, int j) {
+        return (0 <= i && i < m && 0 <= j && j < n);      
     }
 
-    int dx[3] = {-1, 0, 1};
-    int dy[3] = {-1, 0, 1}; 
+    int cntLiveNeighbor(vector<vector<int>>& board, int a, int b) {
+        int i, j, neighbor, cnt = 0;
+        for(i=-1; i<2; i++) {
+            for(j=-1; j<2; j++) {
+                if(!(i | j)) continue;
+        
+                if(isValid(a+i, b+j)) neighbor = board[a+i][b+j];
+                else continue;
+
+                if(neighbor >= 1) cnt++;
+            }
+        }
+        return cnt;
+    }
 
     int nextState(vector<vector<int>>& board, int a, int b) {
-        int m = board.size(), n = board[0].size(), i, j, neighbor_x, neighbor_y, neighbor, cnt = 0;
-
+        int cnt = cntLiveNeighbor(board, a, b);
         if(board[a][b]) {
-            for(i=-1; i<2; i++) {
-                for(j=-1; j<2; j++) {
-                    if(i == 0 && j == 0) continue;
-                    neighbor_x = a + i;
-                    neighbor_y = b + j;
-                    
-                    if(isValid(neighbor_x, neighbor_y, m, n)) {
-                        neighbor = board[neighbor_x][neighbor_y];
-                    }
-                    else continue;
-
-                    if(neighbor >= 1) cnt++;
-                }
-            }
-
             if(cnt == 2 || cnt == 3) return 2; // next state is live
-            else return 3; // next state is die
+            else return 3;                     // next state is die
         }
         else {
-            for(i=-1; i<2; i++) {
-                for(j=-1; j<2; j++) {
-                    if(i == 0 && j == 0) continue;
-                    neighbor_x = a + i;
-                    neighbor_y = b + j;
-                    
-                    if(isValid(neighbor_x, neighbor_y, m, n)) {
-                        neighbor = board[neighbor_x][neighbor_y];
-                    }
-                    else continue;
-
-                    if(neighbor >= 1) cnt++;
-                }
-            }
-            if(cnt == 3) return -2;
-            else return -3;
+            if(cnt == 3) return -2; // next state is live
+            else return -3;         // next state is live
         }
     }
 
     void gameOfLife(vector<vector<int>>& board) {
+        m = board.size();
+        n = board[0].size();
+
         for(int i=0; i<board.size(); i++) {
             for(int j=0; j<board[0].size(); j++) {
                 board[i][j] = nextState(board, i, j);
@@ -60,9 +45,18 @@ public:
 
         for(int i=0; i<board.size(); i++) {
             for(int j=0; j<board[0].size(); j++) {
-                if(abs(board[i][j]) == 2) board[i][j] = 1;
-                else if(abs(board[i][j]) == 3) board[i][j] = 0;
+                board[i][j] = 3 - abs(board[i][j]);
             }
         }
     }
 };
+
+/*
+The cell is initially in live state(1), 
+    if it lives, then it is converted to 2
+    if it dies, then it isconverted to 3
+
+The cell is initially in die state(0), 
+    if it lives, then it is converted to -2
+    if it dies, then it isconverted to -3
+*/
