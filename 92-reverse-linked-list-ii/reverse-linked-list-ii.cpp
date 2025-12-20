164044021a83f991dadf0c888d832b96da5aed98
Tanvir Mahamood <deltatanvir2002@gmail.com>
2025-12-20 20:43:38 +0300

    Time: 0 ms (100.00%) | Memory: 11.3 MB (37.20%) - LeetSync

---
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(left == right) return head;
+
+        ListNode* HEAD = new ListNode(0, head);
+        ListNode*  TAIL(0);
+        ListNode* curr = HEAD;
+        ListNode* num1;
+        ListNode* num2;
+        ListNode* num3;
+        ListNode* lft;
+        ListNode* prev_left;
+        int i, cnt = 0, len = right - left; 
+
+        while(curr->next) {
+            if(cnt == left - 1) {
+                prev_left = curr;
+                lft = curr->next;
+            }
+            curr = curr->next;
+            cnt++;
+        }
+        curr->next = TAIL;
+
+        num1 = lft;
+        num2 = lft->next;
+        num3 = lft->next->next;
+        
+        for(i=0; i<len-1; i++) {
+            num2->next = num1;
+            num1 = num2;
+            num2 = num3;
+            num3 = num3->next;
+        }
+
+        num2->next = num1;
+
+        prev_left->next = num2;
+        lft->next = num3;
+        // else lft->next = nullptr;
+
+
+        return HEAD->next;
+    }
+};
