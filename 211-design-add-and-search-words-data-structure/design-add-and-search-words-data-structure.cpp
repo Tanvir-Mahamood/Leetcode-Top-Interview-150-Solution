0d24744932a6db918b6b21a5e1bbeead14661b17
Tanvir Mahamood <deltatanvir2002@gmail.com>
2026-01-24 06:35:58 +0300

    Time: 535 ms (39.70%) | Memory: 605.5 MB (25.04%) - LeetSync

---
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,82 @@
+struct Node {
+    Node* link[26];
+    bool flag = false;
+
+    bool containKey(char ch) {
+        return (link[ch-'a'] != NULL);
+    }
+
+    bool containIdx(int i) {
+        return (link[i] != NULL);
+    }
+
+    void put(char ch, Node* reff) {
+        link[ch-'a'] = reff;
+    }
+
+    Node* get(char ch) {
+        return link[ch-'a'];
+    }
+
+    void setEnd() {
+        flag = true;
+    }
+
+    bool isEnd() {
+        return flag;
+    }
+};
+
+class WordDictionary {
+private:
+    Node* root;
+
+    bool RecursiveSearch(Node* node, string word, int idx) {
+        bool result;
+        if(idx == word.length()) {
+            return node->isEnd();
+        }
+
+        char ch = word[idx];
+        if(ch == '.') {
+            for(char ch1='a'; ch1<='z'; ch1++) {
+                if(node->containKey(ch1)) {
+                    result = RecursiveSearch(node->get(ch1), word, idx+1);
+                    if(result) return result;
+                }
+            }
+            return false;
+        }
+        else if(node->containKey(ch)) {
+            return RecursiveSearch(node->get(ch), word, idx+1);
+        }
+        else return false;
+    }
+
+public:
+    WordDictionary() {
+        root = new Node();
+    }
+    
+    void addWord(string word) {
+        Node* node = root; 
+        for(char ch: word) {
+            if(!node->containKey(ch)) {
+                node->put(ch, new Node());
+            }
+            node = node->get(ch); 
+        }
+        node->setEnd();
+    }
+    
+    bool search(string word) {
+        return RecursiveSearch(root, word, 0);
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
