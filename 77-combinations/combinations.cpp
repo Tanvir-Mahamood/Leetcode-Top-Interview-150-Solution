f66a688ac907bf7a8c805d1a4ea21b13dd9fa37d
Tanvir Mahamood <deltatanvir2002@gmail.com>
2026-01-26 17:00:27 +0300

    Time: 371 ms (5.01%) | Memory: 196 MB (5.75%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -3,24 +3,22 @@ public:
     vector<vector<int>> ans;
 
     void helper(int level, vector<int> temp, int num, int n, int k) {
-        int next_level = level + 1;
+        if(num != 0) temp.push_back(num);
 
-        if(level != -1) temp.push_back(num);
-
-        if(next_level >= k) {
+        if(level+1 >= k) {
             ans.push_back(temp);
             return;
         }
 
         for(int i=num+1; i<=n; i++) {
-            helper(next_level, temp, i, n, k);
+            helper(level+1, temp, i, n, k);
         }
     }
 
     vector<vector<int>> combine(int n, int k) {
         vector<int> temp;
         int num = 0;
-        helper(-1, temp, num, n, k);
+        helper(-1, temp, 0, n, k);
         return ans;
     }
 };
