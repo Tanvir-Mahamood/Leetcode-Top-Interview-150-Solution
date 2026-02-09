e60e79415cf36089cf7bc30599bb2b14791c0a70
Tanvir Mahamood <deltatanvir2002@gmail.com>
2026-02-09 17:58:28 +0600

    Time: 0 ms (100%), Space: 13.2 MB (86.12%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        vector<int> bitCnt(32, 0);
-        int ans = 0;
-
+        vector<int> cnt(32, 0);
         for(int num: nums) {
-            int j = 0;
-            while(num > 0) {
-                bitCnt[j++] += (num & 1);
-                num = num >> 1;
+            bitset<32> bin(num);
+            for(int i=0; i<32; i++) {
+                cnt[i] = cnt[i] + (bin[i] ? 1 : 0);
             }
         }
+
         for(int i=0; i<32; i++) {
-            bitCnt[i] %= 3;
-            ans += (bitCnt[i] * (1 << i));
+            cnt[i] %= 3;
         }
+
+        bitset<32> arr;
+        for(int i=0; i<32; i++) {
+            arr[i] = cnt[i];
+        }
+        int ans = (int)arr.to_ulong();
 
         return ans;
     }
