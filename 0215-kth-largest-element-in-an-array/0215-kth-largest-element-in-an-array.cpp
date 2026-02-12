7b2ea2bd897700d2b54049c0c30780294c92ef74
Tanvir Mahamood <deltatanvir2002@gmail.com>
2026-02-12 12:00:08 +0600

    Time: 38 ms (34.28%), Space: 64.7 MB (16.89%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> heap;
-        for(int num: nums) {
-            heap.push(num);
-            if(heap.size() > k) {
+        int n = nums.size();
+        if(k <= n/2) {
+            priority_queue<int> heap; // Max heap
+            for(int num: nums) {
+                heap.push(num);
+            }
+            k -= 1;
+            while(k--) {
                 heap.pop();
             }
+            return heap.top();
         }
-        return heap.top();
+        else {
+            priority_queue<int, vector<int>, greater<int>> heap; // Min Heap
+            for(int num: nums) {
+                heap.push(num);
+            }
+            k = n - k;
+            while(k--) {
+                heap.pop();
+            }
+            return heap.top();
+        }
+        return -1; // never happens
     }
 };
