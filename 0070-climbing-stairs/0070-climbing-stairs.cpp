2dee158d2b66e9803f5f10ae1ad0d08cc3851932
Tanvir Mahamood <deltatanvir2002@gmail.com>
2026-02-16 11:39:17 +0600

    Time: 0 ms (100%), Space: 7.9 MB (54.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    int climbStairs(int n) { // Tabulation
+// Tabulation
+/*
+    int climbStairs(int n) { 
         vector<int> dp(n+1, 0);
         dp[0] = dp[1] = 1;
         for(int i=2; i<=n; i++) {
@@ -8,4 +10,30 @@ public:
         }
         return dp[n];
     }
+*/
+
+// Space Optimised Tabulation
+    int climbStairs(int n) {
+        if(n <= 3) return n;
+
+        int first = 2;
+        int second = 3;
+        int curr;
+        for(int i=4; i<=n; i++) {
+            curr = first + second;
+            first = second;
+            second = curr;
+        }
+        return curr;
+    }
 };
+
+/*
+Tabulation
+Time: O(n)
+Space: O(n)
+
+Space Optimised Tabulation
+Time: O(n)
+Space: O(1)
+*/
