656d6b2cc317f3983618ffd200a414ce4a450833
Tanvir Mahamood <deltatanvir2002@gmail.com>
2026-02-22 15:25:38 +0600

    Time: 0 ms (100%), Space: 11.9 MB (30.19%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     bool isvalid(vector<vector<int>>& obstacleGrid, int i, int j) {
-        int row = obstacleGrid.size();
-        int col = obstacleGrid[0].size();
         if(i < 0 || j < 0 || obstacleGrid[i][j] == 1) return false;
         return true;
     }
 
     int solver(vector<vector<int>>& obstacleGrid, int x, int y, vector<vector<int>>& mem) {
-        if(x == 0 && y == 0) return 1;
         if(mem[x][y] != -1) return mem[x][y];
-
+        
         int wayFromLeft = (isvalid(obstacleGrid, x, y-1)) ? solver(obstacleGrid, x, y-1, mem) : 0;
         int wayFromTop =  (isvalid(obstacleGrid, x-1, y)) ? solver(obstacleGrid, x-1, y, mem) : 0;
         return mem[x][y] = wayFromLeft + wayFromTop;
@@ -21,7 +18,7 @@ public:
         int col = obstacleGrid[0].size();
         if(obstacleGrid[row-1][col-1] == 1) return 0;
         vector<vector<int>> mem(row, vector<int>(col, -1));
-        mem[0][0] = 0;
+        mem[0][0] = 1;
 
         return solver(obstacleGrid, row-1, col-1, mem);
     }
