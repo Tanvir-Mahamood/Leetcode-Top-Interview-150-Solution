6e0dccc7d5eff9a80bc99b1ed4ea60e164a7992e
Tanvir Mahamood <deltatanvir2002@gmail.com>
2026-03-07 16:40:16 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,9 +1,9 @@
-//=========================== Trie Template =======================
+//===================== Trie Template ================================
 
 struct Node {
     Node *links[26];
     bool flag = false;
-    string word = ""; // store full word
+    string word = "";
 
     bool ContainsKey(char ch) {
         return (links[ch-'a'] != NULL);
@@ -27,70 +27,75 @@ class Trie
 {
 public:
     Node* root;
-
     Trie() {
-        root = new Node(); //creating new object
+        root = new Node(); // creating new object
     }
 
-    void Insert(string word) { //O(len(word))
-        Node* node = root; //initializing empty node pointing to root initially
+    void Insert(string word) { // O(len(word))
+        Node* node = root; // initializing empty node pointing to root initially
         for(int i=0; i<word.size(); i++) {
             if(!node->ContainsKey(word[i])) {
                 node->put(word[i], new Node());
             }
-            node = node->get(word[i]); //Moves to the reference trie
+            node = node->get(word[i]); // Moves to the reference trie
         }
         node->setEnd(word);
     }
 };
 
-//==================================================================
+//====================================================================
 
 class Solution {
-public:
-    int dx[4] = {1,-1,0,0};
-    int dy[4] = {0,0,1,-1};
+private:
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
 
-    void dfs(vector<vector<char>>& board, int i, int j, Node* node, vector<string>& ans) {
-        char ch = board[i][j];
+    bool isValid(int x, int y, vector<vector<char>>& board) {
+        int row = board.size();
+        int col = board[0].size();
 
-        if(ch == '#' || !node->ContainsKey(ch))
+        if(x < 0 || x >= row || y < 0 || y >= col) return false;
+        if(board[x][y] == '#') return false;
+        return true;
+    }
+
+    void dfs(vector<vector<char>>& board, int x, int y, Node* node, vector<string> &ans) {
+        char ch = board[x][y];
+
+        if(!node->ContainsKey(ch)) {
             return;
+        }
 
         node = node->get(ch);
 
         if(node->isEnd()) {
             ans.push_back(node->word);
-            node->flag = false; // avoid duplicate
+            node->flag = false; // avoid duplication
         }
 
-        board[i][j] = '#';
+        board[x][y] = '#'; // marking as visited
 
-        int row = board.size();
-        int col = board[0].size();
+        for(int i=0; i<4; i++) {
+            int new_x = x + dx[i];
+            int new_y = y + dy[i];
 
-        for(int d=0; d<4; d++) {
-            int ni = i + dx[d];
-            int nj = j + dy[d];
-
-            if(ni>=0 && nj>=0 && ni<row && nj<col) {
-                dfs(board, ni, nj, node, ans);
+            if(isValid(new_x, new_y, board)) {
+                dfs(board, new_x, new_y, node, ans);
             }
         }
-
-        board[i][j] = ch;
+        board[x][y] = ch; // restoration
     }
 
+public:
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        int row = board.size();
+        int col = board[0].size();
+        vector<string> ans;
+
         Trie trie;
         for(string word: words) {
             trie.Insert(word);
         }
-
-        vector<string> ans;
-        int row = board.size();
-        int col = board[0].size();
-        // vector<vector<bool>> visited(row,vector<bool>(col, false));
 
         for(int i=0; i<row; i++) {
             for(int j=0; j<col; j++) {
